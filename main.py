ee23e2771cbdab0b376239fbe04c98ad7deacef7
Fabian Mooslechner <fabian@mooslechner.dev>
2025-12-19 22:30:13 +0100

    Add basic fastapi server

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
+import uvicorn
+
 from pipelineFramework import PipelineServer
 from fastapi import FastAPI
 
+API_BASE_URL = "/api/scraper"
+app = FastAPI(openapi_url=API_BASE_URL + "/openapi.json", docs_url=API_BASE_URL + "/docs", redoc_url=API_BASE_URL + "/redoc")
 
-app = FastAPI()
 
-
-@app.get("/")
+@app.get(API_BASE_URL + "/")
 async def root():
     return {"message": "Hello World"}
 
 
 def main():
+    uvicorn.run(app, host="0.0.0.0", port=8050)
     server = PipelineServer()
     server.start_server()
 
