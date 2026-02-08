b5add793e65babecb75f901914c98f72733371be
Fabian Mooslechner <fabian@mooslechner.dev>
2026-02-08 16:05:26 +0100

    Add pipeline test config

---
+++ pipeline_config.py
@@ -0,0 +1,41 @@
+from typing import Union, List, Dict
+
+from pipelineFramework.server.pipeline.config import PipelineConfig, StepConfig
+
+
+class TestStep(StepConfig):
+    async def run(self):
+        yield
+        print('This is a test step')
+
+    def name(self) -> str:
+        return 'test'
+
+    def display_name(self):
+        return 'Test Step'
+
+    def dependencies(self) -> Union[List[str], None]:
+        return None
+
+
+class TestStep2(StepConfig):
+    async def run(self):
+        yield
+        print('This is another test step')
+
+    def name(self) -> str:
+        return 'test2'
+
+    def display_name(self):
+        return 'Test Step'
+
+    def dependencies(self) -> Union[List[str], None]:
+        return None
+
+PIPELINE_CONFIGS: Dict[str, PipelineConfig] = {
+    'testConfig': {
+        'name': 'testConfig',
+        'display_name': 'Test Config',
+        'steps': [TestStep(), TestStep2()],
+    }
+}
