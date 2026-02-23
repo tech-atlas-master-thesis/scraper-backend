acc2d71a8135edf3b6d3eec5f15c75df9a605321
Fabian Mooslechner <fabian.mooslechner@gmail.com>
2026-02-23 09:57:29 +0100

    Update __init__.py

--- pipeline_configs/__init__.py
+++ pipeline_configs/__init__.py
@@ -2,5 +2,6 @@ from typing import List
 
 from pipelineFramework.server.pipeline.config import PipelineConfig
 from pipeline_configs.test import TEST_PIPELINE
+from pipeline_configs.demo import DEMO_PIPELINE
 
 PIPELINE_CONFIGS: List[PipelineConfig] = [TEST_PIPELINE, DEMO_PIPELINE]
