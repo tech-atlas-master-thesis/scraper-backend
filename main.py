5ce3ffaff16b9d857ad91169152ce4cb4a3b1fa6
Fabian Mooslechner <fabian@mooslechner.dev>
2026-06-10 21:36:41 +0200

    Add first draft for schedules

--- main.py
+++ main.py
@@ -16,5 +16,5 @@ API_BASE_URL = "/api/scraper"
 app = FastAPI(
     openapi_url=API_BASE_URL + "/openapi.json", docs_url=API_BASE_URL + "/docs", redoc_url=API_BASE_URL + "/redoc"
 )
-pipeline_server: PipelineServer = PipelineServer()
-add_common_api_calls(app, pipeline_server, PIPELINE_CONFIGS, CONFIG_DEFINITIONS, API_BASE_URL)
+pipeline_server: PipelineServer = PipelineServer(PIPELINE_CONFIGS, CONFIG_DEFINITIONS)
+add_common_api_calls(app, pipeline_server, API_BASE_URL)
